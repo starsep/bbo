06ed120d70f3bee657ff64e874fe08c7ecf2723b
Filip Czaplicki <filipczaplicki@gmail.com>
2017-10-03 13:00:02 +0200

    Constraints for north/south.

--- generate.sh
+++ generate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mkdir -p dealer_syntax/north dealer_syntax/south
 for c in constraints/*.txt; do
-	filename=`echo "$c" | cut - -f 2 -d /`
-	./converter < "constraints/$filename" > "dealer_syntax/$filename"
+    filename=$(echo "$c" | cut - -f 2 -d /)
+	./converter north < "constraints/$filename" > "dealer_syntax/north/$filename"
+	./converter south < "constraints/$filename" > "dealer_syntax/south/$filename"
 done
